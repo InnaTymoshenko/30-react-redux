f2f56847dfc17fcfc7468fbd470873eac58258f7
Inna Tymoshenko <innatymosh@gmail.com>
2024-03-26 12:20:48 +0200

    Initialize Redux, add and display books

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,16 +1,22 @@
 import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addBook } from '../../redux/books/actionCreaters'
 
 import './BookForm.css'
 
 const BookForm = () => {
 	const [title, setTitle] = useState('')
 	const [author, setAuthour] = useState('')
+	const dispatch = useDispatch()
 
 	const handleSubmit = e => {
 		e.preventDefault()
 		if (title && author) {
-			// dispatch.action
-			console.log(title, author)
+			const book = {
+				title,
+				author
+			}
+			dispatch(addBook(book))
 			setTitle('')
 			setAuthour('')
 		}
