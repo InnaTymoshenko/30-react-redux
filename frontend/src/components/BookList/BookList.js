9db7411499d6ccaa5c0540543be3be00bfb4240f
Inna Tymoshenko <innatymosh@gmail.com>
2024-03-27 11:38:48 +0200

    Filter by author

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,13 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { BsBookmarkStar, BsBookmarkStarFill } from 'react-icons/bs'
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreaters'
-import { selectTitleFilter } from '../../redux/slices/filterSlice'
+import { selectTitleFilter, selectAuthorFilter } from '../../redux/slices/filterSlice'
 
 import './BookList.css'
 
 const BookList = () => {
 	const books = useSelector(state => state.books)
 	const titleFilter = useSelector(selectTitleFilter)
+	const authorFilter = useSelector(selectAuthorFilter)
 	const dispatch = useDispatch()
 
 	const handleDeleteBook = id => {
@@ -20,8 +21,9 @@ const BookList = () => {
 
 	const filteredBooks = books.filter(book => {
 		const matchesTitle = book.title.toLowerCase().includes(titleFilter.toLowerCase())
+		const matchesAuthor = book.author.toLowerCase().includes(authorFilter.toLowerCase())
 		// console.log({ title: book.title, matchesTitle })
-		return matchesTitle
+		return matchesTitle && matchesAuthor
 	})
 
 	return (
--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,14 +1,24 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { setTitleFilter, selectTitleFilter, resetFilters } from '../../redux/slices/filterSlice'
+import {
+	setTitleFilter,
+	selectTitleFilter,
+	setAuthorFilter,
+	selectAuthorFilter,
+	resetFilters
+} from '../../redux/slices/filterSlice'
 
 import './Filter.css'
 
 const Filter = () => {
 	const dispatch = useDispatch()
 	const titleFilter = useSelector(selectTitleFilter)
+	const authorFilter = useSelector(selectAuthorFilter)
 
 	const handleTitleFilterChange = e => {
 		dispatch(setTitleFilter(e.target.value))
+	}
+	const handleAuthorFilterChange = e => {
+		dispatch(setAuthorFilter(e.target.value))
 	}
 
 	const handleResetFilters = () => {
@@ -20,6 +30,9 @@ const Filter = () => {
 			<div className="filter-row">
 				<div className="filter-group">
 					<input value={titleFilter} onChange={handleTitleFilterChange} type="text" placeholder="Filter by title" />
+				</div>
+				<div className="filter-group">
+					<input value={authorFilter} onChange={handleAuthorFilterChange} type="text" placeholder="Filter by author" />
 				</div>
 				<button type="button" onClick={handleResetFilters}>
 					Reset Filters
